f2a8fe0a7aa4f5950e528ed2f9d6bc3731599d11
Md Rehanul haque <rehan78khan87@gmail.com>
2023-05-22 15:30:40 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('form')
+const day = document.getElementById('day')
+const months = document.getElementById('month')
+const years = document.getElementById('year')
+const errorMsg = document.getElementById('error-msg')
+const h4 = document.querySelectorAll('h4')
+const input = document.querySelectorAll('input')
+
+const setDate = ({currentDate, currentMonth, currentYear}) =>{
+    day.innerText = currentDate
+    months.innerText = currentMonth
+    years.innerText = currentYear
+}
+
+const currentTime = {
+    currentDate : new Date().getDate(),
+    currentMonth : new Date().getMonth(),
+    currentYear : new Date().getFullYear()
+}
+
+setDate(currentTime)
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const givenDate = e.target.elements.givenDate.value
+    const givenMonth = e.target.elements.givenMonth.value
+    const givenYear = e.target.elements.givenYear.value
+
+    if(givenDate == '' && givenMonth == '' && givenYear == ''){
+        errorMsg.classList.remove('hidden')
+        h4.forEach(e => e.classList.add('text-red-700'))
+        input.forEach(e => e.classList.add('border-red-700'))
+        return errorMsg.innerHTML = 'This field is required'
+    }
+    else if (!(givenDate <= 30 && givenMonth <= 12)){
+        errorMsg.classList.remove('hidden')
+        h4.forEach(e => e.classList.add('text-red-700'))
+        input.forEach(e => e.classList.add('border-red-700'))
+        return errorMsg.innerHTML = 'Must be a valid date'
+    }
+
+    const calculatedDate = currentTime.currentDate - givenDate
+    const calculatedMonth = currentTime.currentMonth - givenMonth
+    const calculatedYear = currentTime.currentYear - givenYear
+    setDate({currentDate : calculatedDate, currentMonth : calculatedMonth, currentYear : calculatedYear})
+})
+
